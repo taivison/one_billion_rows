31308d1d46529b693e28748e33127e3d82cecd02
Taivison Martins Leite <taivison@gmail.com>
2025-12-04 20:54:57 -0300

    naive implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust Solution for One Billion Row Challenge
+
+Original challenge in <https://github.com/gunnarmorling/1brc>
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::{
+    collections::{BTreeMap, btree_map::Entry},
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    path: PathBuf,
+}
+
+struct Record {
+    count: usize,
+    total: f64,
+    min: f64,
+    max: f64,
+}
+
+impl Record {
+    pub fn new(count: usize, total: f64, min: f64, max: f64) -> Self {
+        Self {
+            count,
+            total,
+            min,
+            max,
+        }
+    }
+
+    pub fn update(&mut self, value: f64) {
+        self.count += 1;
+        self.max = self.max.max(value);
+        self.min = self.min.min(value);
+        self.total += value;
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let file = File::open(args.path)?;
+    let reader = BufReader::new(file);
+
+    let mut map: BTreeMap<String, Record> = BTreeMap::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let (station, temperature) = line
+            .split_once(';')
+            .ok_or(anyhow::anyhow!("Invalid separator"))?;
+
+        let temperature: f64 = temperature.trim().parse()?;
+
+        match map.entry(station.to_string()) {
+            Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(Record::new(1, temperature, temperature, temperature));
+            }
+            Entry::Occupied(ref mut occupied_entry) => occupied_entry.get_mut().update(temperature),
+        }
+    }
+
+    print!("{{");
+
+    let mut stats = map.iter().peekable();
+
+    while let Some((station, record)) = stats.next() {
+        print!(
+            "{station}={}/{}/{}",
+            record.min,
+            record.total / record.count as f64,
+            record.max
+        );
+
+        if stats.peek().is_some() {
+            print!(", ");
+        }
+    }
+
+    print!("}}");
+
+    Ok(())
+}
